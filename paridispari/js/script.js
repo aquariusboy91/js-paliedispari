2390ec592b1ffb54fe93c2fc83d770b70fb80ff1
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2021-11-29 15:11:34 +0100

    add pari e dispari function

--- paridispari/js/script.js
+++ paridispari/js/script.js
@@ -15,24 +15,64 @@
 
 //SENZA FUNZIONE
 
+// let choice = prompt ("pari o dispari?");
+// console.log(choice);
+
+// let user_number =  parseInt(prompt("scegli un numero da 1 a 5"));
+
+// console.log(user_number);
+
+// let pc_number = Math.floor(Math.random() * 6);
+
+// console.log(pc_number);
+
+// totale = user_number + pc_number;
+// console.log(totale);
+
+// if (choice == "pari" && totale % 2 == 0) {
+//     console.log("hai vinto");
+// } else if (choice == "dispari" && totale % 2 == 1){
+//     console.log("hai vinto");
+// } else {
+//     console.log ("hai perso");
+// }
+
+
+//CON FUNZIONE
+
 let choice = prompt ("pari o dispari?");
-console.log(choice);
 
 let user_number =  parseInt(prompt("scegli un numero da 1 a 5"));
 
 console.log(user_number);
 
-let pc_number = Math.floor(Math.random() * 6);
+function rand() {
+    let num_rand =  Math.floor(Math.random() * 6);
+    return num_rand;
+}
 
+let pc_number = rand();
 console.log(pc_number);
 
-totale = user_number + pc_number;
+let totale = user_number + pc_number;
 console.log(totale);
 
-if (choice == "pari" && totale % 2 == 0) {
+
+//CHECK PARI O DISPARI
+function ped(totale) {
+    if (totale % 2 == 0){
+        return ("pari");
+    } else if (totale % 2 == 1) {
+        return ("dispari"); 
+    }
+}
+
+let check = ped(totale);
+console.log(check);
+
+//CONDIZIONE PER IL RISULTATO
+if (check == choice) {
     console.log("hai vinto");
-} else if (choice == "dispari" && totale % 2 == 1){
-    console.log("hai vinto");
-} else {
+} else if (check != choice) {
     console.log ("hai perso");
 }
